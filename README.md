e876998a4d90019c719bb7647d00922bf573022c
AlexRoman777 <alexandru.roman@yh.nackademin.se>
2024-04-15 13:01:16 +0200

    favicon

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Kahoot
-Offline Kahoot!
+# Offline Kahoot
+
+## What is this?
+
+This is a digital version of the game where the teacher asks a question and students answer by raising a card. With this app, there's no need to search for the right card - simply tap the screen, raise your phone, and show your answer.
+
+## How to use it?
+
+Visit the [Offline Kahoot](https://alexroman777.github.io/Kahoot/) webapp and you're all set!
+
+<p align="center">
+    <img src="img/kahoot.png" alt="Kahoot" width=70%>
+</p>
--- docs/script.js
+++ docs/script.js
@@ -1,16 +1,27 @@
 function scrollToShape(color) {
-    const sectionId = `${color}Section`;
-    const section = document.getElementById(sectionId);
+  const sectionId = `${color}Section`;
+  const section = document.getElementById(sectionId);
 
-    if (section) {
-        section.scrollIntoView({ behavior: "smooth" });
-    }
+  if (section) {
+    section.scrollIntoView({ behavior: "smooth" });
+  }
 }
 
 function scrollToSection(sectionId) {
-    const section = document.getElementById(sectionId);
+  const section = document.getElementById(sectionId);
 
-    if (section) {
-        section.scrollIntoView({ behavior: "smooth" });
-    }
+  if (section) {
+    section.scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(function (registration) {
+      console.log("Service Worker registered with scope:", registration.scope);
+    })
+    .catch(function (error) {
+      console.log("Service Worker registration failed:", error);
+    });
 }
